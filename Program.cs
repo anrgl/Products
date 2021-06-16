87b5759e0704c18cfd6c0bb89e50b176649bc2a2
Aynur Galimov <galimovaynur@protonmail.com>
2021-06-16 19:51:43 +0500

    add methods for generating products to .json

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Products.Models;
 
 namespace Products
 {
@@ -6,7 +9,28 @@ namespace Products
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, Product> products = GenerateProductsStore(1000);
+            MakeJSON(products, "products.json");
+        }
+
+        private static void MakeJSON(Dictionary<int, Product> products, string fineName)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products, options);
+            File.WriteAllText(fineName, jsonString);
+        }
+
+        private static Dictionary<int, Product> GenerateProductsStore(int prodCount)
+        {
+            Dictionary<int, Product> products = new Dictionary<int, Product>(prodCount);
+            for (int i = 1; i < prodCount + 1; i++)
+            {
+                products.Add(i, Product.Of(i, "Product #" + i));
+            }
+            return products;
         }
     }
 }
